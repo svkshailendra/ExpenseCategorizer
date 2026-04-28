9e10260dac5cf11023560ab7821dcca78f39b685
shailendra.svk <svkshailendra@gmail.com>
2026-04-28 21:21:37 +0530

    cleanup

--- ExpenseCategorizer.Client/Program.cs
+++ ExpenseCategorizer.Client/Program.cs
@@ -10,47 +10,6 @@ namespace ExpenseCategorizer
     {
         public static async Task Main(string[] args)
         {
-            //var builder = WebAssemblyHostBuilder.CreateDefault(args);
-            //builder.RootComponents.Add<App>("#app");
-            //builder.RootComponents.Add<HeadOutlet>("head::after");
-
-            //////builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:7071/api/") });
-            ////builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://expensefunctiondemo-h4afcqhnfqbceqht.australiaeast-01.azurewebsites.net/api/") });
-
-
-            //builder.Services.AddScoped<ExpenseService>();
-            ////builder.Logging.SetMinimumLevel(LogLevel.Debug);
-            ////builder.Logging.AddConsole();
-
-            ////Add MSAL authentication
-            //builder.Services.AddMsalAuthentication(options =>
-            //{
-            //    //builder.Configuration.Bind("AzureAd", options.ProviderOptions.Authentication);
-
-            //    // Request the API scope you exposed
-            //    //options.ProviderOptions.DefaultAccessTokenScopes.Add(
-            //    //    "api://abac5165-ffcf-46dd-9659-98fce7cbc6d8/user_impersonation");
-
-            //    options.ProviderOptions.Authentication.Authority = "https://login.microsoftonline.com/16f6111d-ab26-4e29-85fe-6e700ea29f7f";
-            //    options.ProviderOptions.Authentication.ClientId = "a81c2799-74ae-4849-a497-3032d71b34b1";
-
-            //    // options.ProviderOptions.DefaultAccessTokenScopes.Add("api://abac5165-ffcf-46dd-9659-98fec7cbc6d8/user_impersonation");
-
-            //});
-
-            //// Configure HttpClient to use the authenticated handler
-            //builder.Services.AddHttpClient("ExpenseCategorizer.API",
-            //    client => client.BaseAddress = new Uri("https://expensefunctiondemo-h4afcqhnfqbceqht.australiaeast-01.azurewebsites.net/api/"))
-            //    .AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
-
-            //// Supply HttpClient instances for injection
-            //builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>()
-            //    .CreateClient("ExpenseCategorizer.API"));
-
-            //builder.Services.AddScoped<ExpenseService>();
-
-            //await builder.Build().RunAsync();
-
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
             builder.RootComponents.Add<HeadOutlet>("head::after");
@@ -73,24 +32,11 @@ namespace ExpenseCategorizer
                 options.ProviderOptions.Authentication.ClientId =
                     "a81c2799-74ae-4849-a497-3032d71b34b1";
 
-                options.ProviderOptions.Authentication.PostLogoutRedirectUri = "https://localhost:7045/authentication/logout-callback";
-
+                options.ProviderOptions.Authentication.PostLogoutRedirectUri = "https://purple-water-04d7b3300.7.azurestaticapps.net/authentication/logout-callback";
                 // Request the API scope you exposed
-                options.ProviderOptions.DefaultAccessTokenScopes.Add(
-                    "abac5165-ffcf-46dd-9659-98fce7cbc6d8/user_impersonation");
+                options.ProviderOptions.DefaultAccessTokenScopes.Add("abac5165-ffcf-46dd-9659-98fce7cbc6d8/user_impersonation");
             });
-            //builder.Services.AddMsalAuthentication(options =>
-            //{
-            //    builder.Configuration.Bind("AzureAd", options.ProviderOptions);
-
-            //    options.ProviderOptions.DefaultAccessTokenScopes.Add(
-            //        "api://abac5165-ffcf-46dd-9659-98fec7cbc6d8/user_impersonation");
-            //});
-
-            // Configure HttpClient to use the authenticated handler
-            //builder.Services.AddHttpClient("ExpenseCategorizer.API",
-            //    client => client.BaseAddress = new Uri("https://expensefunctiondemo-h4afcqhnfqbceqht.australiaeast-01.azurewebsites.net/api/"))
-            //    .AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
+            
             builder.Services.AddHttpClient("ExpenseCategorizer.API",
                                 client => client.BaseAddress = new Uri("https://expensefunctiondemo-h4afcqhnfqbceqht.australiaeast-01.azurewebsites.net/api/"))
                             .AddHttpMessageHandler(sp => sp.GetRequiredService<AuthorizationMessageHandler>()
@@ -98,11 +44,7 @@ namespace ExpenseCategorizer
                                 authorizedUrls: new[] { "https://expensefunctiondemo-h4afcqhnfqbceqht.australiaeast-01.azurewebsites.net" },
                                 scopes: new[] { "abac5165-ffcf-46dd-9659-98fce7cbc6d8/user_impersonation" }));
 
-            ////builder.Services.AddHttpClient("ExpenseCategorizer.API",
-            ////    client => client.BaseAddress = new Uri("http://localhost:7071/api/"))
-            ////    .AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
-
-
+            
             // Supply HttpClient instances for injection
             builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>()
                 .CreateClient("ExpenseCategorizer.API"));
